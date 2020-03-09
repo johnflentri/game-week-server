d1f3121b46519a4787e1499eec3e5da6dcf77df0
johnflentri <jpflentri@gmail.com>
2020-03-09 11:13:32 +0100

    Added test log

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express()
 
 const port = 4000 || process.env.PORT
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, console.log('Database schema connected to port:', port))
 
