c4ffadc7b6becb2a8c1b048540bff726536137c4
johnflentri <jpflentri@gmail.com>
2020-03-12 16:47:21 +0100

    Only imageboard code

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const userRouter = require('./user/router')
-const scoreRouter = require('./scores/router')
 const authRouter = require('./auth/router')
-const messageRouter = require('./message/router')
-const channelRouter = require('./channel/router')
 const imageRouter = require('./image/router')
-const stream = require('./stream')
-const db = require('./db')
 
 const port = process.env.PORT || 4000
 
@@ -20,31 +15,7 @@ app.use(parserMiddleware)
 
 app.use(authRouter)
 app.use(userRouter)
-app.use(scoreRouter)
-app.use(channelRouter)
 app.use(imageRouter)
-
-app.get('/stream', (request, response) => {
-  stream.init(request, response)
-
-  const action = {
-    type: 'ALL_MESSAGES',
-    payload: db.messages
-  }
-  stream.send(action)
-
-  const channelAction = {
-    type: 'ALL_CHANNELS',
-    payload: db.channels
-  }
-
-  stream.send(channelAction)
-})
-
-app.use(messageRouter)
-app.use(channelRouter)
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log('Database schema connected to port:', port))
 
